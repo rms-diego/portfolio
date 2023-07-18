b626b3acc9e77a8e965cf03c038260ca775da1b7
Diego Ramos <diegorbrito9@gmail.com>
2023-07-18 16:41:42 -0300

    new: criando estrutura basica do projeto

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.scss";
+
+import type { Metadata } from "next";
+import { Work_Sans } from "next/font/google";
+
+const workSans = Work_Sans({ subsets: ["latin"], weight: ["300", "600"] });
+
+export const metadata: Metadata = { title: "Portfolio" };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={workSans.className}>{children}</body>
+    </html>
+  );
+}
