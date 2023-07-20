2b0014fe386f3fac042b2f2c2e96be9a924bc5e1
Diego Ramos <diegorbrito9@gmail.com>
2023-07-20 15:07:25 -0300

    feat: criando a home page
    description:
    criando o componente da home page
    criando arquivo de estilo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import styles from "./home.module.scss";
+
 export default function Home() {
-  return <main>Hello world!</main>;
+  return (
+    <main className={styles.mainContainer}>
+      <h2>
+        Sou um <span>Desenvolvedor fullstack</span> especializado no{" "}
+        <span>Back end.</span>
+      </h2>
+    </main>
+  );
 }
