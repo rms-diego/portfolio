3e91463e2268fcc4d9d57905e043c8dfbc9a190a
Diego Ramos <diegorbrito9@gmail.com>
2023-09-19 15:55:25 -0300

    feat: configurando imagens e adicionando o axios no projeto

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        pathname:
+          "/wikipedia/commons/thumb/a/a7/React-icon.svg/800px-React-icon.svg.png",
+      },
 
-module.exports = nextConfig
+      {
+        protocol: "https",
+        hostname: "opengraph.githubassets.com",
+        // pathname: "1/rms-diego/*",
+      },
+    ],
+  },
+};
+
+// http://localhost:3000/(https://opengraph.githubassets.com/1/rms-diego/portfolio)
+
+module.exports = nextConfig;
