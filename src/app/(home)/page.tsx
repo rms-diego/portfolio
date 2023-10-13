abb37cc158f75268aff4d4a94af0988257988d75
Diego Ramos <diegorbrito9@gmail.com>
2023-10-12 22:43:10 -0300

    feat: adicionando fiveicon

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,15 @@
-import { Metadata } from "next";
-import styles from "./home.module.scss";
+import { Metadata } from 'next';
+import styles from './home.module.scss';
 
 export const metadata: Metadata = {
-  title: "Home",
+  title: 'Home',
 };
 
 export default function Home() {
   return (
     <main className={styles.mainContainer}>
       <h2 className="animate__animated animate__backInDown">
-        Sou um <span>Desenvolvedor fullstack</span> especializado no{" "}
-        <span>Back end.</span>
+        Sou um <span>Desenvolvedor fullstack</span>
       </h2>
     </main>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
-import { Footer } from "@/components/Footer";
-import "./globals.scss";
+import { Footer } from '@/components/Footer';
+import './globals.scss';
 
-import { Header } from "@/components/Header";
-import type { Metadata } from "next";
-import { Work_Sans } from "next/font/google";
+import { Header } from '@/components/Header';
+import type { Metadata } from 'next';
+import { Work_Sans } from 'next/font/google';
 
-const workSans = Work_Sans({ subsets: ["latin"], weight: ["300", "600"] });
+const workSans = Work_Sans({ subsets: ['latin'], weight: ['300', '600'] });
 
-export const metadata: Metadata = { title: "Portfolio" };
+export const metadata: Metadata = { title: 'Portfolio' };
 
 export default function RootLayout({
   children,
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <link rel="shortcut icon" href="/logo.png" type="image/x-icon" />
+      </head>
       <body className={workSans.className}>
         <Header />
         {children}
